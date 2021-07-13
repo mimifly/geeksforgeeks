c496903e53512e2534755ec8a9364ded6a4ee46c
mimifly <codemenerd@gmail.com>
2021-07-13 11:51:14 +0100

    Initial solution

---
+++ first-non-repeating-array-element.js
@@ -0,0 +1,18 @@
+const array = [1,2,3,4,1,1,1,3,3,2]
+
+function firstNonRepeat(array) {
+  let j = 0;
+  let tally = {}
+  for (let a of array) {
+    if (tally[a]) {
+      tally[a]++;
+      while(tally[array[j]] > 1) j++
+    }
+    else {
+       tally[a] = 1
+    }   
+  }
+  return array[j]
+}
+
+firstNonRepeat(array)
