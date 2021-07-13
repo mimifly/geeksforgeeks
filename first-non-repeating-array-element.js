f0e6a5c31b5a0656f1ec1066616d0cd14f9134b8
mimifly <codemenerd@gmail.com>
2021-07-13 11:54:53 +0100

    Add comments

--- first-non-repeating-array-element.js
+++ first-non-repeating-array-element.js
@@ -1,14 +1,16 @@
-const array = [1,2,3,4,1,1,1,3,3,2] //test array
+const array = [1,2,3,4,1,1,1,3,3,2] // test array
 
 function firstNonRepeat(array) {
-  let j = 0;
-  let tally = {}
+  let j = 0; // keeps track of the index for the current element that we haven't found a repeat for
+  let tally = {} // object to keep track of the tally for each element as we loop through array
+  
+  // necessary to loop through every element in case of edge case scenarios where an element will repeat on the last index
   for (let a of array) {
-    if (tally[a]) {
+    if (tally[a]) { // element already exists in the tally object
       tally[a]++;
       while(tally[array[j]] > 1) j++
     }
-    else {
+    else { // initialiasing the element in the tally object
        tally[a] = 1
     }   
   }
